8734a963c2f2a80eeb91fe16e038fc5ef1623b27
Spencer Markin <smarkin@questica.com>
2020-05-05 07:41:42 -0400

    open win32 window

--- code/win32_sudoku.c
+++ code/win32_sudoku.c
@@ -1,11 +1,104 @@
 #include <windows.h>
 
+HWND NewWindowHandle(HINSTANCE instance, LPCSTR className) 
+{
+	return CreateWindowEx(
+			0,
+			className,
+			"Sudoku",
+			WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			0, 
+			0, 
+			instance,
+			0);	
+}
+
+void DrawToWindow(HWND window) 
+{
+	PAINTSTRUCT paint;
+	HDC device_context = BeginPaint(window, &paint);
+
+	int x = paint.rcPaint.left;
+	int y = paint.rcPaint.top;
+	LONG w = paint.rcPaint.right - paint.rcPaint.left;
+	LONG h = paint.rcPaint.bottom - paint.rcPaint.top;
+
+	PatBlt(device_context, x, y, w, h, WHITENESS);
+
+	EndPaint(window, &paint);
+}
+
+LRESULT CALLBACK
+MainWindowCallback(
+	HWND window,
+	UINT message,
+	WPARAM w_param,
+	LPARAM l_param
+) 
+{
+	LRESULT result;
+
+	switch(message) {
+		case WM_SIZE: 
+			OutputDebugString("WM_SIZE\n");
+			break;
+		case WM_DESTROY:
+			OutputDebugString("WM_DESTROY\n");
+			break;
+		case WM_ACTIVATEAPP:
+			OutputDebugString("WM_ACTIVATEAPP\n");
+			break;
+		case WM_PAINT:
+			DrawToWindow(window);
+			break;
+		default:
+			result = DefWindowProc(window, message, w_param, l_param);
+			break;
+	}
+
+	return result;
+}
+
 int CALLBACK
 WinMain(
 	HINSTANCE instance,
 	HINSTANCE previous_instance,
 	LPSTR command_line,
-	int show_code)
+	int show_code) 
 {
+
+	WNDCLASS window_class = {0};
+	
+	window_class.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
+	window_class.lpfnWndProc = MainWindowCallback;
+	window_class.hInstance = instance;
+	window_class.lpszClassName = "SudokuClass";
+
+	if(RegisterClass(&window_class)) {
+		HWND window_handle = NewWindowHandle(instance, window_class.lpszClassName);
+
+		if(window_handle) {
+			MSG message;
+			for(;;) {
+				BOOL message_result = GetMessage(&message, 0 ,0 ,0);
+
+				if(message_result > 0) {
+					TranslateMessage(&message);
+					DispatchMessage(&message);
+				}
+				else {
+					break;
+				}
+			}
+		} else {
+			//TODO(spencer): Logging
+		}
+	}
+
 	return 0;
 }
+
